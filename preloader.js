dae981a73777165e4a6c137497687a4fffc4008c
gusponholz <134299946+gusponholz@users.noreply.github.com>
2024-09-05 17:03:24 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://gusponholz.github.io/preloaderJs/loaderBet.svg';
+svgImage.src = 'https://gusponholz.github.io/preloaderJs/splashNovo.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
